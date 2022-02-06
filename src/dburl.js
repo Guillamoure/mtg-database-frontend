deb190a6570b79d4b684819ee8417e1745333d08
Jack Hilscher <jhilscheriv@gmail.com>
2022-02-06 14:32:50 -0500

    updated database url

--- src/dburl.js
+++ src/dburl.js
@@ -1,6 +1,6 @@
 const dbUrl = () => {
   let url = "http://localhost:3000";
-  // url = "https://characterfinder-api.herokuapp.com";
+  url = "https://shrouded-ridge-40022.herokuapp.com/";
   return url;
 };
 export default dbUrl();
