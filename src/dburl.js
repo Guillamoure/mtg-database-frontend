97c553d04509ab41ea80494f23b0a31417ea4096
Jack Hilscher <jhilscheriv@gmail.com>
2022-02-06 17:31:33 -0500

    changed backend url for prod

--- src/dburl.js
+++ src/dburl.js
@@ -1,6 +1,6 @@
 const dbUrl = () => {
   let url = "http://localhost:3000";
-  // url = "https://shrouded-ridge-40022.herokuapp.com/";
+  url = "https://shrouded-ridge-40022.herokuapp.com/";
   return url;
 };
 export default dbUrl();
