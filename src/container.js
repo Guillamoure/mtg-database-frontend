7592976511fe91ddd6bcad3b5bf06083d40c30bf
Jack Hilscher <jhilscheriv@gmail.com>
2022-02-19 21:26:08 -0500

    reformated container info and design to be more readable

--- src/container.js
+++ src/container.js
@@ -16,12 +16,28 @@ const Container = ({ container, setViewContainer }) => {
         }
       }
     });
-    let stringArray = [];
+
+    let sorted = [];
     for (let set in obj) {
-      stringArray.push(`${Math.round((obj[set] / count) * 100)}% ${set}`);
+      sorted.push([set, obj[set]]);
     }
+    sorted = sorted.sort((a, b) => {
+      return b[1] - a[1];
+    });
+
+    let stringArray = [];
+    sorted.forEach((el) => {
+      let set = el[0];
+      let numOfCards = el[1];
+
+      let percent = Math.round((numOfCards / count) * 100);
+      if (percent === 0) {
+        percent = ">1";
+      }
+      stringArray.push(`${percent}% ${set} (${numOfCards})`);
+    });
     return (
-      <ul>
+      <ul style={{ columnCount: "4" }}>
         {stringArray.map((s) => (
           <li>{s}</li>
         ))}
@@ -34,7 +50,7 @@ const Container = ({ container, setViewContainer }) => {
       <div id="modal-content">
         <h4>{container.name}</h4>
         <p>{container.cards?.length} Cards</p>
-        <p>{percentageSets()}</p>
+        <div>{percentageSets()}</div>
         <button onClick={() => setViewContainer(null)}>Close</button>
       </div>
     </div>
